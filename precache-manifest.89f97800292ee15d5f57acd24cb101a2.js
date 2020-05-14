d0eb29cdd6e34dbff0ac7598fba9454a82b807c5
Mohamed Fawzy <mhmdsalm7777@gmail.com>
2020-05-14 17:14:37 +0200

    Updates

---
+++ precache-manifest.89f97800292ee15d5f57acd24cb101a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0bd435db056da67723de7fe5fc1a8b5",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "cbcb14e194d6425e5a44",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "a05ddee2b0a7c0dc5328",
+    "url": "/robofriends/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "cbcb14e194d6425e5a44",
+    "url": "/robofriends/static/js/2.800020c3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.800020c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a05ddee2b0a7c0dc5328",
+    "url": "/robofriends/static/js/main.bdce3418.chunk.js"
+  },
+  {
+    "revision": "4b54cd1813776aa306f7",
+    "url": "/robofriends/static/js/runtime-main.de469359.js"
+  }
+]);
